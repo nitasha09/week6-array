d593887b473cdc70a8e7abb073087efbdb1393ae
Nitasha Martin <shukshara09@gmail.com>
2021-04-17 15:06:32 +0800

    Add existing file

--- array1/array1/Form1.vb
+++ array1/array1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim strNamaHari(6) As String
+        Dim strNamaHari(7) As String
         Dim n As Integer
 
         strNamaHari(0) = "Ahad"
@@ -10,6 +10,7 @@
         strNamaHari(4) = "Khamis"
         strNamaHari(5) = "Jumaat"
         strNamaHari(6) = "Sabtu"
+
 
 
         For n = 0 To 6
